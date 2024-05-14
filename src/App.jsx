a6960615b8e46664fa9ff817ac744f080524e649
Michael Marconi <michaelziga2@gmail.com>
2024-05-14 14:07:52 -0500

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -32,18 +32,29 @@ function App() {
       </header>
       <h1>𝐁𝐄𝐒𝐓_𝐑𝐀𝐈𝐃𝐄𝐍</h1>
       <div className="card">
-        <div>
+        <div className='left-image'>
+          <img src='https://steamuserimages-a.akamaihd.net/ugc/788609251622526633/4D504407E2D00312040D218470D2C6726ED71E4A/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false'
+            className='espn' alt='espn pic' />
+        </div>
+        <div className='centered-image'>
           <img src={espn} className='espn' alt='espn pic' />
         </div>
+        <div className="right-image">
+        <img src='https://64.media.tumblr.com/962f7035afed668c49a2d763ca70b1d1/80bb3f62b35857e7-a3/s540x810/df09359db5873aa7c50ac6465fa7c8d7f2ca4a15.gifv'
+        className='combo' alt='combo pic' />
 
-        <div className="summary-box">
-          <p className="summary">
-            BEST_RAIDEN HERE! I am a teacher in the gaming world, sharing your knowledge gained from years
-            of professional experience. Your expertise helps others understand gaming concepts easily,
-            making you a sought-after mentor. You're dedicated to helping aspiring game developers learn and grow,
-            making a significant impact on the gaming community.
-          </p>
         </div>
+
+
+      </div>
+
+      <div className="summary-box">
+        <p className="summary">
+          BEST_RAIDEN HERE! I am a teacher in the gaming world, sharing your knowledge gained from years
+          of professional experience. Your expertise helps others understand gaming concepts easily,
+          making you a sought-after mentor. You're dedicated to helping aspiring game developers learn and grow,
+          making a significant impact on the gaming community.
+        </p>
       </div>
       <p className="read-the-docs">
         Click on the Youtube and Twitch logos to see more.
@@ -52,7 +63,7 @@ function App() {
         <button className="submit-request-btn" onClick={toggleForm}>
           Submit a Request
         </button>
-        {showForm && <Form toggleForm={toggleForm} onFormSubmit={handleFormSubmit}/>}
+        {showForm && <Form toggleForm={toggleForm} onFormSubmit={handleFormSubmit} />}
       </div>
       <footer>
         <div class="contact-info">
